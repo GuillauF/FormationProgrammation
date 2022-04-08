4f67d1e76879b4e82cbd3d84d5d95d1ced4ef310
GuillauF <guillaume.faure18@gmail.com>
2022-04-08 10:05:19 +0200

    Modif Projet Hackathlon

--- 1Symfony/FCmillenaire/src/DataFixtures/AppFixtures.php
+++ 1Symfony/FCmillenaire/src/DataFixtures/AppFixtures.php
@@ -5,13 +5,13 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
         $manager->flush();
     }
+
 }
