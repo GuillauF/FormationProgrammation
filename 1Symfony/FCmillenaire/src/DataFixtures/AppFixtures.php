ab82a8b24441ef5d469feb50fb286975ff4f27ff
OrfeoDev <orfe34@icloud.com>
2022-04-08 09:35:42 +0200

    add "faker rajout d'images" diffence entre male et female

--- 1Symfony/FCmillenaire/src/DataFixtures/AppFixtures.php
+++ 1Symfony/FCmillenaire/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Equipe;
 use App\Entity\Joueur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,15 +13,34 @@ class AppFixtures extends Fixture
     {
         $faker = \Faker\Factory::create('fr_FR');
         \Bezhanov\Faker\ProviderCollectionHelper::addAllProvidersTo($faker);
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Placeholder($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Demographic($faker));
 
-        for ($j = 0; $j < 100; $j++) {
-            $joueur = new Joueur;
-            $joueur->setName($faker->firstName())
-                ->setSurname($faker->lastName())
-                ->setMail($faker->email());
-            $manager->persist($joueur);
+
+        for ($e = 0; $e < 10; $e++) {
+
+            $equipe = new Equipe();
+            $equipe->setName(strtoupper($faker->city))
+                ->setJoueur($faker->name);
+
+            $manager->persist($equipe);
+
+            for ($j = 0; $j < mt_rand(15, 20); $j++) {
+                $joueur = new Joueur;
+                $joueur->setName($faker->firstName())
+                    ->setSurname(strtoupper($faker->lastName()))
+                    ->setMail($faker->email())
+                    ->setDescription($faker->sentence(10,true))
+                    ->setPicture($faker->imageUrl())
+                    ->setCountry($faker->country())
+                    ->setGenre($faker->randomElement($array=array('male','female')))
+                    ->setEquipe($equipe);
+
+                $manager->persist($joueur);
+
+
+            }
         }
-
 
 
         // $product = new Product();
